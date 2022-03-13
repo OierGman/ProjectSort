0d702ba0c08c2c60006903413025293ea5dc94fb
Oier Gonzalez <o.menesesgonzalez@canterbury.ac.uk>
2022-03-13 00:34:07 +0000

    completed buble sort method

--- ProjectSort/Form1.Designer.cs
+++ ProjectSort/Form1.Designer.cs
@@ -29,20 +29,71 @@ namespace ProjectSort
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxArrayLength = new System.Windows.Forms.TextBox();
+            this.buttonAccept = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonSort.Location = new System.Drawing.Point(0, 546);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(1262, 61);
+            this.buttonSort.TabIndex = 0;
+            this.buttonSort.Text = "Sort Graph";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select The Length of Your List";
+            // 
+            // textBoxArrayLength
+            // 
+            this.textBoxArrayLength.Location = new System.Drawing.Point(218, 5);
+            this.textBoxArrayLength.Name = "textBoxArrayLength";
+            this.textBoxArrayLength.Size = new System.Drawing.Size(100, 22);
+            this.textBoxArrayLength.TabIndex = 3;
+            // 
+            // buttonAccept
+            // 
+            this.buttonAccept.Location = new System.Drawing.Point(324, 6);
+            this.buttonAccept.Name = "buttonAccept";
+            this.buttonAccept.Size = new System.Drawing.Size(75, 23);
+            this.buttonAccept.TabIndex = 4;
+            this.buttonAccept.Text = "Accept";
+            this.buttonAccept.UseVisualStyleBackColor = true;
+            this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 607);
+            this.Controls.Add(this.buttonAccept);
+            this.Controls.Add(this.textBoxArrayLength);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSort);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxArrayLength;
+        private System.Windows.Forms.Button buttonAccept;
     }
 }
 
--- ProjectSort/Form1.cs
+++ ProjectSort/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,28 +13,83 @@ namespace ProjectSort
 {
     public partial class Form1 : Form
     {
+        /*
+        int Min = 0;
+        int Max = 20;
+
+        int[] test2 = new int[5];
+        Random randNum = new Random();
+        foreach (int value in test2)
+        {
+            randNum.Next(Min, Max);
+        }
+        */
+        int p;
+        int[] unsorted_list;
+
         PictureBox pictureBox1 = new PictureBox();
         public void CreateBitmapAtRuntime()
         {
-            pictureBox1.Size = new Size(410, 210);
+            pictureBox1.Size = new Size(1000, 1000);
             this.Controls.Add(pictureBox1);
 
-            Bitmap flag = new Bitmap(400, 200);
-            Graphics flagGraphics = Graphics.FromImage(flag);
-            int red = 0;
-            int white = 11;
-            while (white <= 100) {
-            flagGraphics.FillRectangle(Brushes.Red, 0, red, 400,10);
-            flagGraphics.FillRectangle(Brushes.White, 0, white, 400, 10);
-            red += 20;
-            white += 20;
+            Bitmap graph = new Bitmap(1000, 1000);
+            Graphics graphGraphics = Graphics.FromImage(graph);
+            int y = 0;
+            int x = 300 / unsorted_list.Length;
+            foreach (int i in unsorted_list) 
+            {
+                graphGraphics.FillRectangle(Brushes.Red, y, 35, x, i * 5);
+                y += x + 2;
             }
-            pictureBox1.Image = flag;
+            pictureBox1.Image = graph;
         }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+
+            for (int write = 0; write < unsorted_list.Length; write++)
+            {
+                for (int sort = 0; sort < unsorted_list.Length - 1; sort++)
+                {
+                    if (unsorted_list[sort] > unsorted_list[sort + 1])
+                    {
+                        temp = unsorted_list[sort + 1];
+                        unsorted_list[sort + 1] = unsorted_list[sort];
+                        unsorted_list[sort] = temp;
+                        // Thread.Sleep(250);
+                    }
+                    CreateBitmapAtRuntime();
+                    pictureBox1.Update();
+                }
+            }
+        }
+
+        private void buttonAccept_Click(object sender, EventArgs e)
+        {
+            bool pValue = Int32.TryParse(textBoxArrayLength.Text, out int tempIntP);
+
+            if (pValue == true)
+            {
+                p = tempIntP;
+            }
+            int Min = 1;
+            int Max = 70;
+
+            unsorted_list = new int[p];
+            Random randNum = new Random();
+            for (int i = 0; i < unsorted_list.Length; i++)
+            {
+                unsorted_list[i] = randNum.Next(Min, Max);
+            }
             CreateBitmapAtRuntime();
+            pictureBox1.Update();
         }
     }
 }
